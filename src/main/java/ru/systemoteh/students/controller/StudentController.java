05fb27426102245a48aef757ec98014eba306bd3
systemoteh <aleksei_0888@mail.ru>
2019-12-26 23:20:47 +0300

    studentDetail

--- src/main/java/ru/systemoteh/students/controller/StudentController.java
+++ src/main/java/ru/systemoteh/students/controller/StudentController.java
@@ -2,13 +2,20 @@ package ru.systemoteh.students.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StudentController {
 
-    @RequestMapping("/students")
-    public String giveAllStudents(Model model) {
+    @GetMapping("/students")
+    public String getAllStudents(Model model) {
         return "students";
+    }
+
+    @GetMapping(value = "/student/{studentUid}")
+    public String getStudentById(@PathVariable String studentUid, Model model) {
+        return "studentDetail";
     }
 }
