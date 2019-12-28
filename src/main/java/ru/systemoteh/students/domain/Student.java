fbfdfdeeeba05d63b496d98ff7d98418043acb34
systemoteh <aleksei_0888@mail.ru>
2019-12-28 13:58:26 +0300

    domain class

---
+++ src/main/java/ru/systemoteh/students/domain/Student.java
@@ -0,0 +1,27 @@
+package ru.systemoteh.students.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "first_name", length = 250, nullable = false)
+    private String firstName;
+    @Column(name = "middle_name", length = 250)
+    private String middleName;
+    @Column(name = "last_name", length = 250, nullable = false)
+    private String lastName;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "birth_date", nullable = false)
+    private Date birthDate;
+    @Column(name = "email", length = 250)
+    private String email;
+}
